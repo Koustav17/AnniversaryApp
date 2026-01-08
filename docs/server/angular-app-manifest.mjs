0b1292fd6f105f613b1ce4012f47de428cb20694
Koustav17 <koustav33@gmail.com>
2026-01-08 23:00:17 +0530

    Phone icon & text chnage new

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1225, hash: 'ab840e04d2f81e08a2d46b59ccfe6bfa799e1c49a239a15123d5a46fc7fa7b0c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1083, hash: '14eb224112f774217cedad5b7c2baee734e53481538b059b7a97c86f2d938295', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 5284, hash: '8d006b6d74056f43168a9789f7bf10e71da6c0881b74d56292159b61ea648eec', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1225, hash: 'f4f2535bd34a0ce0b31285abab8d0c18efb17fdb1a4399adebea9c23a63e3c0c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1083, hash: 'a193b9940bf8842a70da3a07806028e51f67438410d109f5e92c60fdf0645e50', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 5296, hash: 'c4f05339df0dc19dfaf2088ad8d076b239003dc49f11fef43d5d87c26472a427', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-V232HVLG.css': {size: 2897, hash: 'HZ8b381CNFs', text: () => import('./assets-chunks/styles-V232HVLG_css.mjs').then(m => m.default)}
   },
 };
