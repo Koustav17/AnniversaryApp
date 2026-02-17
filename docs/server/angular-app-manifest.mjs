7c0c51efb5c81267f95b323c942eeda73b7711d8
Koustav17 <koustav33@gmail.com>
2026-02-17 23:06:20 +0530

    Going Live Css Change

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1225, hash: 'eca8fc38f5f21f01a16e0c8dfeaf8203fe5f999be4e2c1768f77b955fa1155b6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1083, hash: '5bd7f1ef9995052affc9472a7f93fbd2a144de39107e7e516452f88664c08774', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 5297, hash: 'a0010022362a24aa2999402d1010e1e64fe32b3b438394132396363f8bce4486', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1225, hash: '416f92cb6f4a8d85330561e30ab9d23f59e1e044f202e7f392ec36cb4ae8c922', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1083, hash: 'cd25e85a2cdca9225fd434f5a8a9155f31817f138bdb085bd0bdca1c12c3eb54', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 5583, hash: '9165136e2bac3babdfe5705eb51874f23b74afa13e113beb2e1fe2728c59db84', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-V232HVLG.css': {size: 2897, hash: 'HZ8b381CNFs', text: () => import('./assets-chunks/styles-V232HVLG_css.mjs').then(m => m.default)}
   },
 };
